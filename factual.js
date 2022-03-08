fcc0ee56d7eeaab0ee60f2d68f05b694c11fd028
Daniel <daniel1120morandi@gmail.com>
2022-03-07 20:09:38 -0600

    Fact check in js

--- factual.js
+++ factual.js
@@ -1,71 +1,54 @@
+//imports
 const fs = require('fs');
 const axios = require('axios');
 const https=require('https');
 const bodyParse=require('body-parser');
 const express =require("express");
-let text; 
+var text; 
 const app=express();
 
 
+var claim="Trump lost the 2020 election";
+var key=key;
 
-//Read in file
-const get_api =async()=>{
-	console.log("Function"); 
-	fs.readFile('/home/ragdoll/ACM_Projects/data.txt',async (err, data)=>  {
-		//Throws error if file does not exist
-		if (err){
-			 throw err;
-		}
-		 console.log("continue");
 
-		//Converts data to a string tipe
-		text=data.toString();
-		//Replaces /n
-		text=text.replace("\n","");
-		//send output to console
-		console.log("Before"+text);
-		return text;
-	})
-}
-//
+var url="https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-us&maxAgeDays=100&pageSize=5&pageToken=1&query="+claim+"&key="+key;
 
-const set_api=async()=>{
-	const key=await get_api();
-	console.log(key);
-}
-let claim="5g";
-let key=key;
-set_api();
-console.log("After"+key);
-let url="https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en-us&maxAgeDays=100&pageSize=5&pageToken=1&query="+claim+"&key="+key;
-https.get(url,function (res){
-	console.log(res.statusCode);
-	let chunks=[];
+https.get(url,function (res){   
+	//console.log(res.statusCode);
+	var chunks=[];
+	//grab data
 	res.on("data",function(data){
 		chunks.push(data);
-	}).on("end",function(){
-		let jdata=Buffer.concat(chunks);
-		let schema=JSON.parse(jdata);
-		let claims=schema.claims;
-		console.log(schema);
-		console.log("Seperate");
-		console.log(claims);
-		let review=claims[0].claimReview;
-		console.log("Seperate");
-		console.log(review);
-	let myPromise=new Promise(function(myResolve,myReject){
-		let url=review.url;
-		myResolve();
-		myReject();
-	
-	});
-	myPromise.then(
-		console.log("url: "+url)
-	);
-	console.log("Review");
-	console.log(review);
-
 	})
+	
+	
+	res.on("end",function(){
+		var jdata=Buffer.concat(chunks);
+		var schema=JSON.parse(jdata);
+
+		console.log(schema);
+		console.log("\n");
+		//gathers info of claim
+		var claims=schema.claims;
+		
+
+		for(let i=0;i<claims.length;i++){
+		
+			//Grabs one claim
+			var review=claims[i].claimReview;
+			var claimant=claims[i].claimant;
+			//var claimant=schema[i].claimant;
+			console.log(review);
+			review=review[0];
+	
+			//console.log("name: "+review.publisher.name);
+			console.log("Claimant: "+claimant);
+			console.log("site: "+review.publisher.site);
+			console.log("url: "+review.url);
+			console.log("title: "+review.title);
+			console.log("Rating: "+review.textualRating);
+		}
+	})
+	
 })
-
-
